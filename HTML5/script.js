05c98fbe7759c41c4c3eb0e98402a6cb78547694
Farid0811 <94936615+Farid0811@users.noreply.github.com>
2021-11-30 18:16:45 -0500

    commit 3

--- HTML5/script.js
+++ HTML5/script.js
@@ -2,9 +2,11 @@
 console.log("Script loaded!")
 var form1 = document.getElementById("form");
 var content = document.getElementById("form-content");
-
+var mainList = document.getElementById("mainList");
+function getName(id){
+	content.innerHTML+=id;
+}
 form1.addEventListener("submit", function(event){
-
 	event.preventDefault();
 	const data = Object.fromEntries(new FormData(event.target).entries());
 	console.log(event);
@@ -14,11 +16,9 @@ form1.addEventListener("submit", function(event){
 	content.innerHTML += "<p>" + data.name + "</p>";
 	content.innerHTML = "<h1>Number: </h1>";
 	content.innerHTML += "<p>" + data.number + "</p>";
-
   	content.innerHTML += data.password;
-	
-});
 
+});
 function example () {
 
 	return []
